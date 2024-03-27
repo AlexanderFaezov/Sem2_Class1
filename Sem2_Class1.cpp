913f2aec70d737d140838652f14bd210a2f4dc5e
AlexanderFaezov <s.faezov.com@mail.ru>
2024-03-27 09:12:40 +0500

    Добавьте файлы проекта.

---
+++ Sem2_Class1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Progress {
+private:
+    double first;
+    double second;
+    double pown(double x, int y) {
+        cout << "Возводим " << x << " в степень: " << y << endl;
+        double c = pow(x, y);
+        return c;
+    }
+public:
+    void SetFirst(double x) {
+        this->first = x;
+        cout << "a0: " << first << endl;
+    }
+    void SetSecond(int y) {
+        this->second = y;
+        cout << "r: " << second << endl;
+    }
+    double element(double j) {
+        cout << "a0 ( " << first << " )" << " мы умножаем на следующее действие: " << endl;
+        return first * pown(second, j);
+    }
+};
+
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    Progress prog;
+    prog.SetFirst(4);
+    prog.SetSecond(3);
+    cout << "Ответ: " << prog.element(2) << endl;
+    
+    return 0;
+}
